53539f08a403a7d3377cfdc9eb278de89c2593d6
Arkadiusz <Arkadiusz@LAPTOP-RJJ77HA0>
2023-01-20 14:50:26 +0100

    Add project files.

---
+++ Zadanie 5/Controllers/StudentsController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Zadanie_5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> _studenci = new List<Student>();//tu można wpisać bazę pobieraną z SQL Server
+
+        [HttpGet("getStudents")]
+        public ActionResult<IEnumerable<Student>> GetAll()
+        {
+            return _studenci.ToList();
+        }
+
+        [HttpPost("createStudent")]
+        public void Post([FromBody] Student student)
+        {
+            _studenci.Add(student);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Student student)
+        {
+            Student s = _studenci.FirstOrDefault(x => x.IdStudent == id);
+            s.Imie = student.Imie;
+            s.Nazwisko=student.Nazwisko;
+            s.Adres=student.Adres;
+            s.NumerIndeksu=student.NumerIndeksu;
+            s.Email=student.Email;
+        }
+
+        [HttpDelete("delete/{id}")]
+        public void Delete(int id)
+        {
+            Student s = _studenci.FirstOrDefault(x => x.IdStudent == id);
+            _studenci.Remove(s);
+        }
+    }
+}
---
+++ Zadanie 5/Student.cs
@@ -0,0 +1,12 @@
+﻿namespace Zadanie_5
+{
+    public class Student
+    {
+        public int IdStudent { get; set; }   
+        public string Imie { get; set; }    
+        public string Nazwisko { get; set; }
+        public string Adres { get; set; }
+        public string NumerIndeksu { get; set; }
+        public string Email { get; set; }
+    }
+}
